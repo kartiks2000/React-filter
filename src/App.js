15e588421cca0edff92245fd2d8a08f22073c18f
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-15 13:49:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{ useState } from 'react';
 import './App.css';
 import Mobile from './Mobile/Mobile';
+import Aux from './Aux/Aux';
 
 function App() {
 
@@ -112,32 +113,34 @@ const findwirelesscharging = () => {
 
 
   return (
-    <div className="App">
-      <div className="Header">
-        <h1 className="main-heading">Mobile World</h1>
-        <h2 className="sub-heading">A Device can chnage your life...</h2>
-        <div className="filters">
-          <button onClick={showhideall}>show all</button>
-          <button onClick={findwaterproof}>Waterproof</button>
-          <button onClick={findmulticamerasetup}>multicamerasetup</button>
-          <button onClick={findinternationalwarrenty}>internationalwarrenty</button>
-          <button onClick={findretinascan}>retinascan</button>
-          <button onClick={findexynox}>exynox</button>
-          <button onClick={findsnapdragon}>snapdragon</button>
-          <button onClick={findwirelesscharging}>wirelesscharging</button>
-          <button onClick={searchbar}>search</button>
-          {showsearchbar? <input className="block" placeholder="Search" type="text" onChange={findsearch}></input> : null}
-          {required != null? required.map(i => {
-            return <Mobile company={i.company} model={i.model} ram={i.ram} waterproof={i.waterproof} multicamerasetup={i.multicamerasetup} internationalwarrenty={i.internationalwarrenty} retinascan={i.retinascan} exynox={i.exynox} snapdragon={i.snapdragon} gorillaglass={i.gorillaglass} wirelesscharging={i.wirelesscharging} key={i.id} myid={i.id}></Mobile>
-          }) : <h1>Sorry, no match found !!!</h1>}
+    <Aux>
+      <div className="App">
+        <div className="Header">
+          <h1 className="main-heading">Mobile World</h1>
+          <h2 className="sub-heading">A Device can chnage your life...</h2>
+          <div className="filters">
+            <button onClick={showhideall}>show all</button>
+            <button onClick={findwaterproof}>Waterproof</button>
+            <button onClick={findmulticamerasetup}>multicamerasetup</button>
+            <button onClick={findinternationalwarrenty}>internationalwarrenty</button>
+            <button onClick={findretinascan}>retinascan</button>
+            <button onClick={findexynox}>exynox</button>
+            <button onClick={findsnapdragon}>snapdragon</button>
+            <button onClick={findwirelesscharging}>wirelesscharging</button>
+            <button onClick={searchbar}>search</button>
+            {showsearchbar? <input className="block" placeholder="Search" type="text" onChange={findsearch}></input> : null}
+            {required != null? required.map(i => {
+              return <Mobile company={i.company} model={i.model} ram={i.ram} waterproof={i.waterproof} multicamerasetup={i.multicamerasetup} internationalwarrenty={i.internationalwarrenty} retinascan={i.retinascan} exynox={i.exynox} snapdragon={i.snapdragon} gorillaglass={i.gorillaglass} wirelesscharging={i.wirelesscharging} key={i.id} myid={i.id}></Mobile>
+            }) : <h1>Sorry, no match found !!!</h1>}
+          </div>
+          <hr className="line"></hr>
         </div>
-        <hr className="line"></hr>
-      </div>
-      {showall? mobile.map(i => {
-        return <Mobile company={i.company} model={i.model} ram={i.ram} waterproof={i.waterproof} multicamerasetup={i.multicamerasetup} internationalwarrenty={i.internationalwarrenty} retinascan={i.retinascan} exynox={i.exynox} snapdragon={i.snapdragon} gorillaglass={i.gorillaglass} wirelesscharging={i.wirelesscharging} key={i.id} myid={i.id}></Mobile>
-      }) : null }
+        {showall? mobile.map(i => {
+          return <Mobile company={i.company} model={i.model} ram={i.ram} waterproof={i.waterproof} multicamerasetup={i.multicamerasetup} internationalwarrenty={i.internationalwarrenty} retinascan={i.retinascan} exynox={i.exynox} snapdragon={i.snapdragon} gorillaglass={i.gorillaglass} wirelesscharging={i.wirelesscharging} key={i.id} myid={i.id}></Mobile>
+        }) : null }
 
-    </div>
+      </div>
+    </Aux>  
   );
 }
 
